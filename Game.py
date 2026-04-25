a909f26c389b3d76e565cae4600bdfc17f8422d8
Patrick Farrell <farrellp@terpmail.umd.edu>
2026-04-24 20:50:28 -0400

    Added GameConditions class

---
+++ Game.py
@@ -0,0 +1,23 @@
+class GameConditions:
+    
+    """
+    Game conditions class with methods for calculating scores, even/odd rule,
+    blistering, and sixes rule.
+    
+    Author: Terry Thompson
+    
+    """
+    def __init__(self, dice):
+        self.dice = dice
+    
+    def calculate_matches(self):
+        pass
+    
+    def check_even_odd(self):
+        pass
+    
+    def check_blister(self):
+        pass
+    
+    def check_all_sixes(self):
+        pass
