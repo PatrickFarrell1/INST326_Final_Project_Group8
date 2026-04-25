85329cf64577b3e23232bc4fef866450d5abb3fc
Patrick Farrell <farrellp@terpmail.umd.edu>
2026-04-24 20:54:03 -0400

    Added Turn_manager class

---
+++ Skeleton.py
@@ -0,0 +1,21 @@
+class Turn_manager:
+    
+    """
+    Turn manager class that includes methods for determining which player's
+    turn it is and when the game ends.
+    
+    Author: Sheila Matumla
+    """
+    def __init__(self, players, player_index):
+        self.players = players
+        self.player_index = player_index
+# Creates class and define initiation with parameters
+        def next_turn():
+            pass
+# Creates method that would change the turns between players.
+        def rest_turn():
+            pass
+ # Creates method that would rest the turn between the players.           
+        def end_game():        
+            pass
+# Creates method that would end the game. 
