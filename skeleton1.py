bdf6b8e39b4baed67894571c2f564d45d2d8c361
Patrick Farrell <farrellp@terpmail.umd.edu>
2026-04-24 20:57:38 -0400

    Added DiceSet class

---
+++ skeleton1.py
@@ -0,0 +1,28 @@
+class DiceSet:
+    
+    """
+    Dice set class that includes methods for rolling the dice and displaying
+    values.
+    
+    Author: Rachel Galloway
+    """
+    def __init__(self, num_dice=6):
+        self.num_dice = num_dice
+        self.colors = ["red", "blue", "white"]
+        self.dice = [] # stores tuples for color and value
+    
+    def roll_all(self):
+        # rolls all the dice to start the turn
+        pass
+
+    def roll_remaining(self, num_dice):
+        # rolling the remaining dice after the first turn
+        pass
+
+    def display(self):
+        # display the players dice values
+        pass
+
+    def get_values(self):
+        # return the values on the dice
+        pass
