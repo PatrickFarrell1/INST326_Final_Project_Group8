f6a3875ed67dbd7460a074d8f9e2cdbe916e6fe7
Patrick Farrell <farrellp@terpmail.umd.edu>
2026-05-12 15:11:16 -0400

    DiceSet Class: Rachel Galloway

--- skeleton1.py
+++ skeleton1.py
@@ -1,29 +1,133 @@
+import random
+
 class DiceSet:
-    
-    """
-    Dice set class that includes methods for rolling the dice and displaying
-    values.
+    '''
+    The game always contains:
+    - 2 red dice
+    - 2 blue dice
+    - 2 white dice
+
+    Each die has:
+    - a color (red, blue, white)
+    - a value between 1-6
     
     Author: Rachel Galloway
     
-    """
+    '''
+
     def __init__(self, num_dice=6):
+        '''
+        Parameters:
+        num_dice (int): total number of dice in a set
+        '''
+
         self.num_dice = num_dice
         self.colors = ["red", "blue", "white"]
-        self.dice = [] # stores tuples for color and value
-    
+        self.dice = []  # stores tuples of (color, value)
+
     def roll_all(self):
-        # rolls all the dice to start the turn
-        pass
+        '''
+        Roll all 6 dice at the beginning of the turn
+
+        Returns:
+        list: list of tuples containing (color, value)
+        '''
+
+        self.dice = []
+
+        # exactly 2 of each color
+        color_pool = [
+            "red", "red",
+            "blue", "blue",
+            "white", "white"
+        ]
+
+        # randomize the order
+        random.shuffle(color_pool)
+
+        # assign random values
+        for color in color_pool:
+            value = random.randint(1, 6)
+            self.dice.append((color, value))
+
+        return self.dice
 
     def roll_remaining(self, num_dice):
-        # rolling the remaining dice after the first turn
-        pass
+        '''
+        Roll the remaining dice after the first turn
+
+        Parameters:
+        num_dice (int): number of dice to reroll
+
+        Returns:
+        list: updated list of rerolled dice
+        '''
+
+        new_dice = []
+
+        # maintain 2 of each color
+        color_pool = [
+            "red", "red",
+            "blue", "blue",
+            "white", "white"
+        ]
+
+        # randomize colors
+        random.shuffle(color_pool)
+
+        # reroll only requested number of dice
+        for i in range(num_dice):
+            color = color_pool[i]
+            value = random.randint(1, 6)
+
+            new_dice.append((color, value))
+
+        self.dice = new_dice
+
+        return self.dice
 
     def display(self):
-        # display the players dice values
-        pass
+        '''
+        Display the current dice with colored output
+        '''
+
+        # ANSI color codes
+        color_codes = {
+            "red": "\033[31m",
+            "blue": "\033[34m",
+            "white": "\033[37m"
+        }
+
+        reset = "\033[0m"
+
+        print("Current Dice:")
+
+        for color, value in self.dice:
+
+            code = color_codes[color]
+
+            print(
+                f"{code}[{color.upper()} DIE] "
+                f"rolled a {value}{reset}"
+            )
 
     def get_values(self):
-        # return the values on the dice
-        pass
+        '''
+        Return the dice values
+
+        Returns:
+        list: list of tuples containing (color, value)
+        '''
+
+        return self.dice
+
+
+# PLAYER TURN
+
+player_dice = DiceSet()
+
+player_dice.roll_all()
+
+print("PLAYER ROLLED:")
+
+player_dice.display()
